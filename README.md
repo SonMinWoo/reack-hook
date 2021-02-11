a89cfa26d19f8bd27dcb2a2a09bf9066ada4445a
Minwoo Son <mwson98@naver.com>
2021-02-11 23:22:11 +0900

    2-0. useEffect

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # reack-hook
-Created with CodeSandbox
+
+## 1. useState
+
+- useState(arg)는 배열을 return
+- 1st element : useState를 쓸 값
+- 2nd element : useState에 적용되는 함수
+
+## 2. useEffect
+
+- componentDidMount and componentWillUpdate
+- 2개의 parameter를 받음
+- 1st parameter : 실행할 함수
+- 2nd parameter : 실행 조건(dependency)
--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./styles.css";
 import useInput from "./useInput";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const sayHello = () => console.log("hello");
+  const [number, setNumber] = useState(0);
+  const [anumber, setaNumber] = useState(0);
+
+  useEffect(sayHello, []);
   return (
     <div className="App">
       <h1>Hello </h1>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setaNumber(anumber + 1)}>{anumber}</button>
     </div>
   );
 };
